dd919833bc89da268732d2801e291bfdaa50d9f4
Susan Murray <murray.e.susan@gmail.com>
2025-02-06 12:33:03 -0800

    new assertion: 'mySpuDmids' must have only 1 'disturbance_matrix_id' each combination of 'spatial_unit_id' and 'rasterID'

--- CBM_core.R
+++ CBM_core.R
@@ -474,6 +474,12 @@ annual <- function(sim) {
 
   # mySpuDmids was created in CBM_dataPrep_XX
   mySpuDmids <- copy(sim$mySpuDmids)
+
+  # Check that there is one disturbance_matrix_id for each spatial_unit_id and rasterID
+  if (nrow(unique(mySpuDmids[, .(spatial_unit_id, rasterID, disturbance_matrix_id)])) >
+      nrow(unique(mySpuDmids[, .(spatial_unit_id, rasterID)]))) stop(
+        "'mySpuDmids' must have only 1 'disturbance_matrix_id' each combination of 'spatial_unit_id' and 'rasterID'")
+
   mySpuDmids[, "events" := rasterID][, rasterID := NULL]
   cols <- c("spatial_unit_id", "events")
   wholeStandDist <- merge.data.table(distPixels, mySpuDmids, by = cols)
